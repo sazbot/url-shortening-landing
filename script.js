ccb7335fd20101815898e477ea9a204c5b0af10f
Sarah Fielding Smith <s.fieldingsmith@gmail.com>
2022-04-29 11:33:51 +0100

    write js pseudocode

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const nav = document.querySelector(".nav");
 openButton.addEventListener("click", () => {
   nav.classList.toggle("navigation-open");
 });
+
+// add event listener to submit button
+// select form input value
+// set up form validation (add .display-error class to .error-message)
+// add to API base URL
+// set up async await request (w. error handling)
+// extract short URL from JSON data
+// [consider create html template] append original and short URL to .copy-link (& add .display-copy-link)
+// save original and short URLs in local storage and render on refresh
+// add event listener to copy button
+// select closest short URL and copy to clipboard
