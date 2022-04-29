53d11c0729f440ba8815a278afa422aa9f336490
Sarah Fielding Smith <s.fieldingsmith@gmail.com>
2022-04-29 18:36:50 +0100

    add copy clipboard functionality

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ openButton.addEventListener("click", () => {
   nav.classList.toggle("navigation-open");
 });
 
-// add functionlity to shorten URL form
+// add functionality to shorten URL form
 shortenURLForm.addEventListener("submit", () => {
   const inputValue = formInput.value;
   const isValidURL = validateInput(inputValue);
@@ -37,11 +37,19 @@ shortenURLForm.addEventListener("submit", () => {
 // clear error messages
 clearError();
 
-// add event listener to copy button
-// select closest short URL and copy to clipboard
+// add functionality to copy buttons
+shortenURLSection.addEventListener("click", (e) => {
+  if (!e.target.matches("[data-btn-copy]")) return;
+
+  const targetURL = e.target.previousElementSibling.innerText;
+  copyURL(targetURL);
+
+  const targetButton = e.target;
+  targetButton.style.backgroundColor = "#222127";
+  targetButton.innerText = "Copied";
+});
 
 // Function definitions
-
 function validateInput(input) {
   let regexp =
     /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
@@ -101,4 +109,10 @@ function loadLinks() {
 
 function saveLinks() {
   sessionStorage.setItem(LINKS_STORAGE_KEY, JSON.stringify(links));
+}
+
+function copyURL(url) {
+  navigator.clipboard.writeText(url).then((res) => {
+    console.log("url copied to clipboard successfully");
+  });
 }
